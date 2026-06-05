7423ca2869eed77f1136c51e8a0deb351a87d8f2
ntquan1205 <tvquan12345@gmail.com>
2026-06-06 01:21:20 +1000

    Adding basic player movement

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float speed = 5f;
+    public Rigidbody2D rb;
+
+    void FixedUpdate() 
+    {
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
+
+        rb.linearVelocity = new Vector2(horizontal, vertical) * speed;
+    }
+
+}
