af37bde23e5cbbf372f9668191bacf3d7e901574
ntquan1205 <tvquan12345@gmail.com>
2026-06-06 02:54:03 +1000

    Adding SideWalk Animation

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -4,11 +4,15 @@ public class PlayerMovement : MonoBehaviour
 {
     public float speed = 1f;
     public Rigidbody2D rb;
+    public Animator animator;
 
     void FixedUpdate() 
     {
         float horizontal = Input.GetAxis("Horizontal");
         float vertical = Input.GetAxis("Vertical");
+
+        animator.SetFloat("XInput", horizontal);
+        animator.SetFloat("YInput", vertical);
 
         rb.linearVelocity = new Vector2(horizontal, vertical) * speed;
     }
