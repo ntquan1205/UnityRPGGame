0c8a919457c3e0c5f16cef43416a2df80c5dc4a6
ntquan1205 <tvquan12345@gmail.com>
2026-06-06 03:09:54 +1000

    Fixing bug with sildewalk

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -5,11 +5,17 @@ public class PlayerMovement : MonoBehaviour
     public float speed = 1f;
     public Rigidbody2D rb;
     public Animator animator;
+    public SpriteRenderer spriteRenderer; 
 
     void FixedUpdate() 
     {
         float horizontal = Input.GetAxis("Horizontal");
         float vertical = Input.GetAxis("Vertical");
+
+        if (horizontal < 0)
+            spriteRenderer.flipX = true;
+        else
+            spriteRenderer.flipX = false;
 
         animator.SetFloat("XInput", horizontal);
         animator.SetFloat("YInput", vertical);
