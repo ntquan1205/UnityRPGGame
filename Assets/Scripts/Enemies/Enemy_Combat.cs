04f73f8016b2430919f568968c1a499fbf63f6ec
ntquan1205 <tvquan12345@gmail.com>
2026-06-12 00:53:18 +1000

    Adding attack animation to enemy

--- Assets/Scripts/Enemies/Enemy_Combat.cs
+++ Assets/Scripts/Enemies/Enemy_Combat.cs
@@ -5,14 +5,18 @@ public class Enemy_Combat : MonoBehaviour
     public int damage = 1;
 
     private void OnCollisionEnter2D(Collision2D collision)
-{
-    if (collision.gameObject.CompareTag("Player"))
     {
-        PlayerHealth player = collision.gameObject.GetComponent<PlayerHealth>();
-        if (player != null)
+        if (collision.gameObject.CompareTag("Player"))
         {
-            player.ChangeHealth(-damage);
+            PlayerHealth player = collision.gameObject.GetComponent<PlayerHealth>();
+            if (player != null)
+            {
+                player.ChangeHealth(-damage);
+            }
         }
     }
-}
+    public void Attack()
+    {
+        Debug.Log("Attacking player");
+    }
 }
--- Assets/Scripts/Enemies/Enemy_Movement.cs
+++ Assets/Scripts/Enemies/Enemy_Movement.cs
@@ -5,6 +5,7 @@ public class Enemy_Movement : MonoBehaviour
     public float speed;
     private int facingDirection = -1;
     private EnemyState enemyState;
+    public float attackRange = 2;
     private Rigidbody2D rb;
     private Transform player;
     private Animator anim;
@@ -21,13 +22,27 @@ public class Enemy_Movement : MonoBehaviour
     {
         if (enemyState == EnemyState.Chasing)
         {
-            if (player.position.x > transform.position.x && facingDirection == -1 || player.position.x < transform.position.x && facingDirection == 1) 
-            {
-                Flip();
-            }
-            Vector2 direction = (player.position - transform.position).normalized;
-            rb.linearVelocity = direction * speed;   
+            Chase();
         }
+        else if (enemyState == EnemyState.Attacking)
+        {
+            rb.linearVelocity = Vector2.zero;
+        }
+    }
+
+    void Chase()
+    {
+        if (Vector2.Distance(transform.position, player.transform.position) <= attackRange)
+        {
+            ChangeState(EnemyState.Attacking);
+        }
+        else if (player.position.x > transform.position.x && facingDirection == -1 || player.position.x < transform.position.x && facingDirection == 1) 
+        {
+            Flip();
+        }
+        Vector2 direction = (player.position - transform.position).normalized;
+        rb.linearVelocity = direction * speed;  
+        
     }
 
     void Flip()
@@ -35,7 +50,7 @@ public class Enemy_Movement : MonoBehaviour
         facingDirection *= -1;
         transform.localScale = new Vector3(transform.localScale.x * -1,transform.localScale.y, transform.localScale.z);
     }
-    private void OnTriggerEnter2D(Collider2D collision)
+    private void OnTriggerStay2D(Collider2D collision)
     {
         if (collision.gameObject.tag == "Player")
         {
@@ -62,6 +77,8 @@ public class Enemy_Movement : MonoBehaviour
             anim.SetBool("IsIdle", false);
         else if (enemyState == EnemyState.Chasing) 
             anim.SetBool("IsChasing", false);
+        else if (enemyState == EnemyState.Attacking) 
+            anim.SetBool("IsAttacking", false);
 
         enemyState = newState;
 
@@ -69,6 +86,8 @@ public class Enemy_Movement : MonoBehaviour
             anim.SetBool("IsIdle", true);
         else if (enemyState == EnemyState.Chasing) 
             anim.SetBool("IsChasing", true);
+        else if (enemyState == EnemyState.Attacking) 
+            anim.SetBool("IsAttacking", true);
     }
 }
 
@@ -76,4 +95,5 @@ public enum EnemyState
 {
     Idle,
     Chasing,
+    Attacking
 }
