248452acedc3554ff60fb15e34371bccf6a01664
ntquan1205 <tvquan12345@gmail.com>
2026-06-11 14:54:36 +1000

    Adding Health UI

--- Assets/Scripts/PlayerHealth.cs
+++ Assets/Scripts/PlayerHealth.cs
@@ -1,13 +1,22 @@
 using UnityEngine;
+using TMPro;
 
 public class PlayerHealth : MonoBehaviour
 {
     public int currentHealth;
     public int maxHealth;
 
+    public TMP_Text healthText;
+
+    private void Start()
+    {
+        healthText.text = "HP: " + currentHealth + " / " + maxHealth;
+    }
+
     public void ChangeHealth(int amount)
     {
         currentHealth += amount;
+        healthText.text = "HP: " + currentHealth + " / " + maxHealth;
 
         if (currentHealth <= 0)
         {
