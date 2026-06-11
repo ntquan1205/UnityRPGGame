1dff011c0bf662e6ebbbbb7d33b1b4986cef0530
ntquan1205 <tvquan12345@gmail.com>
2026-06-11 12:06:20 +1000

    Adding health system and enemy combat logic

---
+++ Assets/Scripts/Enemies/Enemy_Combat.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+
+public class Enemy_Combat : MonoBehaviour
+{
+    public int damage = 1;
+
+    private void OnCollisionEnter2D(Collision2D collision)
+{
+    if (collision.gameObject.CompareTag("Player"))
+    {
+        PlayerHealth player = collision.gameObject.GetComponent<PlayerHealth>();
+        if (player != null)
+        {
+            player.ChangeHealth(-damage);
+        }
+    }
+}
+}
