887437a5b729c6d13e03426c1ac8b38f616b1fcf
ntquan1205 <tvquan12345@gmail.com>
2026-06-12 12:03:11 +1000

    finishing attacking function

--- Assets/Scripts/Player_Combat.cs
+++ Assets/Scripts/Player_Combat.cs
@@ -2,6 +2,10 @@ using UnityEngine;
 
 public class Player_Combat : MonoBehaviour
 {
+    public Transform attackPoint;
+    public float weaponRange = 1;
+    public LayerMask enemyLayer;
+    public int damage = 1;
     public Animator anim;
     public float cooldown = 2;
     private float timer;
@@ -18,7 +22,18 @@ public class Player_Combat : MonoBehaviour
         if ( timer <= 0)
         {
             anim.SetBool("IsAttacking", true);
+
             timer = cooldown;
+        }
+    }
+
+    public void DealDamage()
+    {
+        Collider2D[] enemies = Physics2D.OverlapCircleAll(attackPoint.position, weaponRange, enemyLayer);
+
+        if (enemies.Length > 0)
+        {
+            enemies[0].GetComponent<Enemy_Health>().ChangeHealth(-damage);
         }
     }
 
